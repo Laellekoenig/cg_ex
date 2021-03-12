e0c8fa984c2f6a5106dd0664200661edececaac7
Maximilian Barth <maximilian1barth@gmail.com>
2021-03-12 13:06:12 +0100

    small code refactoring

--- ex1/src/main/java/utils/BarycentricCoordinateTransform.java
+++ ex1/src/main/java/utils/BarycentricCoordinateTransform.java
@@ -19,27 +19,19 @@ public class BarycentricCoordinateTransform {
   }
 
   public BarycentricCoordinates getBarycentricCoordinates(double x, double y) {
+
+    //TODO: Blatt 1, Aufgabe 3
     double lambdaX = 0, lambdaY = 0, lambdaZ = 0;
 
     assert (!isDegenerate());
 
-    //TODO: Blatt 1, Aufgabe 3
+    lambdaZ = (-1) * ((a.y - b.y) * x   + (b.x - a.x) * y   + a.x * b.y - b.x * a.y) /
+                     ((a.y - c.y) * b.x + (c.x - a.x) * b.y + a.x * c.y - c.x * a.y);
 
-    lambdaZ = (-1) * ((a.y - b.y) * x + (b.x - a.x) * y + a.x * b.y - b.x * a.y) / ((a.y - c.y) * b.x + (c.x - a.x)
-            * b.y + a.x * c.y - c.x * a.y);
-
-    lambdaY = ((a.y - c.y) * x + (c.x - a.x) * y + a.x * c.y - c.x * a.y) / ((a.y - c.y) * b.x + (c.x - a.x)
-            * b.y + a.x * c.y - c.x * a.y);
+    lambdaY =        ((a.y - c.y) * x +   (c.x - a.x) * y   + a.x * c.y - c.x * a.y) /
+                     ((a.y - c.y) * b.x + (c.x - a.x) * b.y + a.x * c.y - c.x * a.y);
 
     lambdaX = 1 - lambdaY - lambdaZ;
-
-    //assert(lambdaX + lambdaY + lambdaZ == 1);
-
-    //assert(x == lambdaX * a.x + lambdaY * b.x + lambdaZ * c.x);
-    //assert(y == lambdaX * a.y + lambdaY * b.y + lambdaZ * c.y);
-
-    //System.out.printf("Should be: (%f, %f), is actually (%f, %f) \n", x, y, lambdaX * a.x + lambdaY * b.x + lambdaZ * c.x
-    //        , lambdaX * a.y + lambdaY * b.y + lambdaZ * c.y);
 
     return new BarycentricCoordinates(lambdaX, lambdaY, lambdaZ);
   }

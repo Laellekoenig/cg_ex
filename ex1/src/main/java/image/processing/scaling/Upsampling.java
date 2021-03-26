1389a29add27b14ab688fd0fae62e0ab5ea068e3
Maximilian Barth <maximilian1barth@gmail.com>
2021-03-26 19:59:00 +0100

    added interpolation upsampeling

--- ex1/src/main/java/image/processing/scaling/Upsampling.java
+++ ex1/src/main/java/image/processing/scaling/Upsampling.java
@@ -20,6 +20,17 @@ public class Upsampling implements ImageAlgorithm {
         new RGBA(0.0f, 0.0f, 0.0f));
 
     //TODO: Blatt 2, Aufgabe 2 b)
+    double stepSize = 0.5;  // factor two
+
+    // go through every pixel
+    for (int y = 0; y < outImg.rows(); y++) {
+      for (int x = 0; x < outImg.cols(); x++) {
+
+        // get nearest color
+        RGBA nearest = interpolation.access(x * stepSize, y * stepSize);
+        outImg.set(x, y, nearest);
+      }
+    }
 
     return outImg;
   }

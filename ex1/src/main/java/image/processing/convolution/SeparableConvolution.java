125e887aae1a2ffda8a1d46bad8939e229a9c369
Maximilian Barth <maximilian1barth@gmail.com>
2021-03-26 18:46:26 +0100

    some fixes in seperable convolution

--- ex1/src/main/java/image/processing/convolution/SeparableConvolution.java
+++ ex1/src/main/java/image/processing/convolution/SeparableConvolution.java
@@ -14,20 +14,31 @@ public abstract class SeparableConvolution extends Convolution {
       throw new IllegalArgumentException("Kernel cannot be a matrix for separable convolution");
     }
 
-    Image<RGBA> halfBaked = new Image<RGBA>(img.cols(), img.rows(), new RGBA(0f, 0f, 0f));
-    Image<RGBA> outImg = new Image<RGBA>(img.cols(), img.rows(), new RGBA(0f, 0f, 0f));
+    Image<RGBA> halfBaked = new Image<RGBA>(img.cols(), img.rows(), new RGBA(0f, 0f, 0f));  // for first convolution
+    Image<RGBA> outImg = new Image<RGBA>(img.cols(), img.rows(), new RGBA(0f, 0f, 0f));     // for second convolution
 
     //TODO: Blatt 2, Aufgabe 1 c)
 
+    // calculate offset for centering kernel on pixel
+    int offset = kernel.cols() / 2;
+
     // apply to rows
+    // go through every pixel of original image
     for (int y = 0; y < img.rows(); y++) {
       for (int x = 0; x < img.cols(); x++) {
 
+        // color to be filled in
         RGBA newColor = new RGBA(0, 0, 0);
+        // apply offset
+        int xStart = x - offset;
 
+        // go through kernel
         for (int i = 0; i < kernel.cols(); i++) {
+          // get value from kernel
           float multiplier = kernel.get(i);
-          RGBA currentColor = img.get(x + i, y);
+          // get current color from image
+          RGBA currentColor = img.get(xStart + i, y);
+          // apply filter
           currentColor = currentColor.times(multiplier);
           newColor = newColor.plus(currentColor);
         }
@@ -37,14 +48,22 @@ public abstract class SeparableConvolution extends Convolution {
     }
 
     // apply to cols
-    for (int x = 0; x < img.cols(); x++) {
-      for (int y = 0; y < img.rows(); y++) {
+    // go through half-baked image
+    for (int x = 0; x < halfBaked.cols(); x++) {
+      for (int y = 0; y < halfBaked.rows(); y++) {
 
+        // color to be set
         RGBA newColor = new RGBA(0, 0, 0);
+        // apply offset
+        int yStart = y - offset;
 
+        // go through kernel
         for (int i = 0; i < kernel.cols(); i++) {
+          // get value from kernel
           float multiplier = kernel.get(i);
-          RGBA currentColor = img.get(x, y + i);
+          // get current color from halfBaked
+          RGBA currentColor = halfBaked.get(x, yStart + i);
+          // apply filter
           currentColor = currentColor.times(multiplier);
           newColor = newColor.plus(currentColor);
         }

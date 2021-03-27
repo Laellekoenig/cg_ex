7a4ae625a1a3dc5870333dfba68e020703e32949
Maximilian Barth <maximilian1barth@gmail.com>
2021-03-27 21:04:58 +0100

    fixed bicubic interpolation

--- ex1/src/main/java/image/processing/scaling/BiCubicInterpolation.java
+++ ex1/src/main/java/image/processing/scaling/BiCubicInterpolation.java
@@ -15,39 +15,53 @@ public class BiCubicInterpolation implements Interpolation {
   @Override
   public RGBA access(double x, double y) {
 
+    // check if calculation necessary
+    if (x - (int) x == 0 && y - (int) y == 0) {
+      // exact point, skip calculation
+      return img.get((int) x, (int) y);
+    }
+
     RGBA res = new RGBA(0.0f, 0.0f, 0.0f);
 
     //TODO: Blatt 2, Aufgabe 2 c)
 
+    // calculate possible x and y values
+    int x0 = (int) x - 1;
+    int x1 = x0 + 1;
+    int x2 = x0 + 2;
+    int x3 = x0 + 3;
+
+    int y0 = (int) y - 1;
+    int y1 = y0 + 1;
+    int y2 = y0 + 2;
+    int y3 = y0 + 3;
+
     // top row, a:
-    RGBA a1 = img.get((int) x - 1,(int) y - 1);
-    RGBA a2 = img.get((int) x, (int) y - 1);
-    RGBA a3 = img.get((int) x + 1, (int) y - 1);
-    RGBA a4 = img.get((int) x + 2, (int) y - 1);
+    RGBA a1 = img.get(x0, y0);
+    RGBA a2 = img.get(x1, y0);
+    RGBA a3 = img.get(x2, y0);
+    RGBA a4 = img.get(x3, y0);
 
     // second row from the top, b:
-    RGBA b1 = img.get((int) x - 1,(int) y);
-    RGBA b2 = img.get((int) x, (int) y);
-    RGBA b3 = img.get((int) x + 1, (int) y);
-    RGBA b4 = img.get((int) x + 2, (int) y);
+    RGBA b1 = img.get(x0, y1);
+    RGBA b2 = img.get(x1, y1);
+    RGBA b3 = img.get(x2, y1);
+    RGBA b4 = img.get(x3, y1);
 
     // third row from the top, c:
-    RGBA c1 = img.get((int) x - 1,(int) y + 1);
-    RGBA c2 = img.get((int) x, (int) y + 1);
-    RGBA c3 = img.get((int) x + 1, (int) y + 1);
-    RGBA c4 = img.get((int) x + 2, (int) y + 1);
+    RGBA c1 = img.get(x0, y2);
+    RGBA c2 = img.get(x1, y2);
+    RGBA c3 = img.get(x2, y2);
+    RGBA c4 = img.get(x3, y2);
 
     // bottom row, d:
-    RGBA d1 = img.get((int) x - 1,(int) y + 2);
-    RGBA d2 = img.get((int) x, (int) y + 2);
-    RGBA d3 = img.get((int) x + 1, (int) y + 2);
-    RGBA d4 = img.get((int) x + 2, (int) y + 2);
+    RGBA d1 = img.get(x0, y3);
+    RGBA d2 = img.get(x1, y3);
+    RGBA d3 = img.get(x2, y3);
+    RGBA d4 = img.get(x3, y3);
 
-    // calculate percentage, total distance = 3
-    // separate decimal numbers first
-    float decimalProgress = (float) x - (int) x;
-    // calculate percentage
-    float percentage = (decimalProgress + 1) / 3;
+    // calculate percentage, get digits after comma   ->  3.456 ->  .456  ->  45.6%
+    float percentage = (float) (x - (int) x);
 
     // interpolate rows
     RGBA a = interpolateCubically(a1, a2, a3, a4, percentage);
@@ -55,9 +69,8 @@ public class BiCubicInterpolation implements Interpolation {
     RGBA c = interpolateCubically(c1, c2, c3, c4, percentage);
     RGBA d = interpolateCubically(d1, d2, d3, d4, percentage);
 
-    // calculate y progress, total distance = 3
-    decimalProgress = (float) y - (int) y;
-    percentage = (decimalProgress + 1) / 3;
+    // calculate y progress, get digits after comma   ->  percentage
+    percentage = (float) (y - (int) y);
 
     // now interpolate over interpolated values
     res = interpolateCubically(a, b, c, d, percentage);
@@ -70,22 +83,23 @@ public class BiCubicInterpolation implements Interpolation {
    * <p>
    * TEACHING: remove this method
    */
-  private RGBA interpolateCubically(RGBA c0, RGBA c1, RGBA c2, RGBA c3, float x) {
+  private RGBA interpolateCubically(RGBA a, RGBA b, RGBA c, RGBA d, float x) {
 
     RGBA res = new RGBA(0.0f, 0.0f, 0.0f);
 
     //TODO: Blatt 2, Aufgabe 2 c)
 
-    //maybe something wrong here
-
     // calculate components
-    RGBA p = (c3.minus(c2)).minus(c0.minus(c1));
-    RGBA q = ((c0.minus(c1)).minus(c3.minus(c2))).times(2);
-    RGBA r = c2.minus(c0);
-    RGBA b = c1;
+    RGBA p = (d.minus(c)).minus(a.minus(b));              // (d - c) - (a -b)
+    RGBA q = ((a.minus(b)).times(2)).minus(d.minus(c));   // 2(a - b) - (d - c)
+    RGBA r = c.minus(a);                                  // c - a
 
-    // polynomial equation
-    res = (((p.times(Math.pow(x, 3))).plus(q.times(Math.pow(x, 2)))).plus(r.times(x))).plus(b);
+    // calculate polynomial
+    RGBA one = p.times(Math.pow(x, 3));   // px^3
+    RGBA two = q.times(Math.pow(x, 2));   // qx^2
+    RGBA three = r.times(x);              // rx
+
+    res = one.plus(two).plus(three).plus(b);
 
     res.clamp();
     return res;

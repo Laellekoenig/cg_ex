116ec499cc171b5f2a38666c1d6069a8770da107
maxjappert <max.jappert@gmail.com>
2021-04-13 12:38:43 +0200

    exercise 1 now works

--- ex1/src/main/java/projection/PinholeProjection.java
+++ ex1/src/main/java/projection/PinholeProjection.java
@@ -40,29 +40,22 @@ public class PinholeProjection extends Projection {
 
     //TODO: Blatt 3, Aufgabe 1
 
-    // C * (R | T)
-    projection = Matrix4.multiply(camera, view);
-    //projection.times(new Vector4(-1, -1/4.0, -1, 1));
-
-    //projection = camera;
-
-    //System.out.printf("camera:\n %s \n", camera);
-    //System.out.printf("view:\n %s \n", view);
-    //System.out.printf("projection:\n %s \n", projection);
+    // For possible debugging:
+    // System.out.printf("camera:\n %s \n", camera);
+    // System.out.printf("view:\n %s \n", view);
+    // System.out.printf("projection:\n %s \n", projection);
 
     // Dieser vierdimensionale Vektor muss auf drei Dimensionen reduziert werden (als Nebenprodukt davon, dass die Multiplikation
     // mit einer 4x4 anstatt einer 3x4 Matrix durchgeführt werden muss. Es gilt x4 = 1, wobei dieser Wert entfernt wird.
     Vector4 homogPt = new Vector4(pt.x, pt.y, pt.z, 1);
 
     Vector4 extProjection = projection.multiply(homogPt);
+    Vector3 result = new Vector3(extProjection.x, extProjection.y, extProjection.z);
+    result = result.times(1.0 / result.z);
 
-    // The projected coordinates are all negative and absolutely high, this patchwork function shows that the solution can't be far!
-    extProjection = extProjection.times(-1 / 3.0);
-
-    //extProjection = extProjection.times(1 / extProjection.w);
-
-    System.out.printf("(%f, %f, %f, %f) \n", extProjection.x, extProjection.y, extProjection.z, extProjection.w);
-    return new Vector3(extProjection.x, extProjection.y, extProjection.z);
+    // System.out.printf("(%f, %f, %f, %f) \n", extProjection.x, extProjection.y, extProjection.z, extProjection.w);
+    // System.out.printf("(%f, %f, %f) \n", result.x, result.y, result.z);
+    return result;
   }
 
   @Override
